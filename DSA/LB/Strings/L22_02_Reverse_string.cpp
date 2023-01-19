7537e7d3003998a6fdcc2815c91ebee9df5cd6f3
Pranav-2103 <113229450+Pranav-2103@users.noreply.github.com>
2023-01-19 15:52:00 +0530

    Add files via upload

---
+++ DSA/LB/Strings/L22_02_Reverse_string.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int length(char a[]) {
+    int count = 0;
+    int i = 0;
+    while (a[i] != '\0') {
+        count++;
+        i++;
+    }
+    return count;
+}
+
+void reverse(char a[], int n) {
+    int s = 0;
+    int e = n-1;
+    while (s<e) {
+        int temp = a[s];
+        a[s] = a[e];
+        a[e] = temp;
+        s++;
+        e--;
+    }
+    cout<<"Reversed string is: "<<a;
+}
+
+int main()
+{
+    char a[10];
+    cin>>a;
+    int size = length(a);
+    reverse(a,size);
+    return 0;
+}
